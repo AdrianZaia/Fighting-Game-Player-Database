2131bf08517e10fc9922bb88a0d3f5e55a131151
Adrian Zaia <46577552+EdZaia@users.noreply.github.com>
2024-11-05 15:21:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Movie Database</h1>
+        <h1>FGC Player Database</h1>
       </header>
       <main>
         <Search />
