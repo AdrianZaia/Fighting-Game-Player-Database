8b9a87e6948d83f6d7d33b61fe1a6ab92ac2ce42
Adrian Zaia <46577552+EdZaia@users.noreply.github.com>
2024-11-05 15:14:01 -0500

    Creation of the FGCDB

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Search () {
+    return (
+        <section className='searchbox-wrap'>
+            <input type='text'placeholder="Search for a PLayer"className='searchbox'>
+            </input>
+        </section>
+    )
+}
+
+export default Search
